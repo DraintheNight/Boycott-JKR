8d8b290fbdc654f230ec17f0a1ab7c4b93cdd619
DrainTheNight <antonpaulpilz@gmail.com>
2026-06-08 21:12:11 +0200

    Git commit!

---
+++ src/a12541406/Potion.java
@@ -0,0 +1,30 @@
+package a12541406;
+
+/**
+ *  A potion has a specific magic effect on its consumer
+ */
+public abstract class Potion extends MagicItem {
+	/**
+	 * @param name name
+	 * @param usages number of usages still left
+	 * @param price price
+	 * @param weight weight
+	 */
+	public Potion(String name, int usages, int price, int weight) {
+	}
+	
+	/**
+	 * Delegates to method call useOn(drinker)
+	 * @param drinker the consumer of the potion
+	 */
+	public void drink(Wizard drinker) {
+	}
+	  
+	/**
+	 * returns "gulp" if usages is equal to 1, "gulps" otherwise
+	 * @return "gulp" or "gulps" depending on number of usages left
+	 */
+	@Override
+	public String usageString() {
+	}
+}
